f8aa12dcb847d7e8b49f99897409cc0ee8e8cc0c
MacroXie <xiehongzhe04@gmail.com>
2024-09-23 22:28:35 -0700

    feat: Add Java Scanner class and JavaScript animation examples
    - Add Java Scanner class and Jupyter Notebook file
    - Implement uniform motion animation in JavaScript
    - Create sidebar animation in JavaScript
    
    Integrate previous notes
    Initial commit

--- Java/src/test.java
+++ Java/src/test.java
@@ -1,4 +1,10 @@
+
 public class test {
+
+    public maionly(){
+        System.out.println("Hello World");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
